52b88aa79de5992f00f3045b825fded265fac71b
mjanecek <mikejanecek@cox.net>
2014-12-16 16:28:51 -0600

    edit and add Assign2 to cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Macro View:  makeCacheMatrix function creates a special "matrix" object
+## cachesolve function computes the inverse of this "matrix object.
+## In addition, if the inverse has already been calculated, then cachsolve function will retrieve this inverse from the cache.
+## Input: e.g. a <- makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2)), a$get(), a$getinvmatrix(), cachesolve(a).
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invmatrix <- NULL
+        set <- function(y) {
+                x <<- y
+                invmatrix <<- NULL
+        }
+        get <- function() {x}
+        setinvmatrix <- function(inverse) {invmatrix <<- inverse}
+        getinvmatrix <- function() {invmatrix}
+        list(set = set, get =get,
+             setinvmatrix = setinvmatrix,
+             getinvmatrix = getinvmatrix)
 }
 
+## cachesolve function
+## calls makeCacheMatrix to see if inverse has already been stored in cache.
+## Message if it is already stored(not NULL) and retrieves stored inverse.
 
-## Write a short comment describing this function
+cachesolve <- function(x, ...) { 
+        
+        invmatrix <- x$getinvmatrix()               
+        if(!is.null(invmatrix)) {               
+                
+                message("getting cached data")  
+                return(invmatrix)                       
+                
+        }
+        ## Calculate the inverse matrix as part of cachesolve.
+        ## Only reach this if x$getinvmarix is NULL.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        data <- x$get()                 
+        invmatrix <- solve(data, ...)            
+        
+        x$setinvmatrix(invmatrix)                    
+        invmatrix                                 
+}   
